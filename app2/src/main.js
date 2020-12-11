611047caa3b44713961dde6adb066c382edff774
Sigrid Bygdas <sigrid.bygdas@halogen.no>
2020-12-11 10:46:43 +0100

    initial

---
+++ app2/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
+
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+        },
+      });
+    },
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
